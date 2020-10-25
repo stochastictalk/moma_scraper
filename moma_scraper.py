02066fc42bef43a32f419fc5b9dcddf8954acf3b
Jerome <jerome.wynne18@ic.ac.uk>
2020-10-25 21:33:06 +0000

    Define function to retrieve metadata for a single item

--- moma_scraper.py
+++ moma_scraper.py
@@ -1,6 +1,6 @@
 # -*- utf-8 -*-
 # Created: 25th October 2020
-# Author: Jeorme Wynne
+# Author: Jerome Wynne
 # Summary: functions for grabbing images from the MoMA website.
 
 import bs4 as bs
@@ -15,6 +15,13 @@ def retrieve_item(n: int):
     soup = bs.BeautifulSoup(r.text, 'html.parser')
 
     work_tag = soup.find('section', attrs={'class':'work'})
-    work_short_caption_tags = tag.find('h1').findAll('span')
-    soup.find('dl', {'class':'work__caption'})
-    return soup
+    work_tag_text = [v.text for v in work_tag.find('h1').findAll('span')]
+    img_url = work_tag.find(
+                        'picture').findAll('source')[-1]['srcset'].split(' ')[0]
+    caption_keys = [t.text for t in
+                            soup.findAll('dt', {'class':'work__caption__term'})]
+    caption_values = [t.text for t in
+                     soup.findAll('dd', {'class':'work__caption__description'})]
+    caption_dct = {caption_keys[j]:caption_values[j] for j in
+                                                       range(len(caption_keys))}
+    return work_tag_text, img_url, caption_dct
