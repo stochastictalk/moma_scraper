321e81a65ce92c8b98e17521e65b45bc28a8e9ea
Jerome <jerome.wynne18@ic.ac.uk>
2020-10-27 07:46:27 +0000

    Define download_to_disk, need to handle JSON list open and close

--- moma_scraper.py
+++ moma_scraper.py
@@ -5,6 +5,9 @@
 
 import bs4 as bs
 import requests
+from os import mkdir
+from shutil import rmtree
+import json
 
 def retrieve_item(n: int):
     ''' Gets image and metadata from https://www.moma.org/collection/works/n.
@@ -21,43 +24,61 @@ def retrieve_item(n: int):
     src_url = 'https://www.moma.org/collection/works/' + str(n)
     r = requests.get(src_url, headers=headers)
 
-    # parse into soup
-    soup = bs.BeautifulSoup(r.text, 'html.parser')
+    if r.status_code == 404:
+        pass
+    else:
+        # parse into soup
+        soup = bs.BeautifulSoup(r.text, 'html.parser')
 
-    # get metadata from header
-    work_tag = soup.find('section', attrs={'class':'work'})
-    meta_work_v = [v.text for v in work_tag.find('h1').findAll('span')]
-    meta_work_k = ['Artist', 'Work', 'Year']
-    meta_work_dct = {k:v for k, v in zip(meta_work_k, meta_work_v)}
+        # get metadata from header
+        work_tag = soup.find('section', attrs={'class':'work'})
+        meta_work_v = [v.text for v in work_tag.find('h1').findAll('span')]
+        meta_work_k = ['Artist', 'Work', 'Year']
+        meta_work_dct = {k:v for k, v in zip(meta_work_k, meta_work_v)}
 
-    # get image url
-    img_url = work_tag.find(
-                        'picture').findAll('source')[-1]['srcset'].split(' ')[0]
-    img_url = 'https://www.moma.org' + img_url
+        # get image url
+        img_url = work_tag.find(
+                    'picture').findAll('source')[-1]['srcset'].split(' ')[0]
+        img_url = 'https://www.moma.org' + img_url
 
-    # get metadata from caption
-    meta_caption_k = [t.text for t in
+        # get metadata from caption
+        meta_caption_k = [t.text for t in
                             soup.findAll('dt', {'class':'work__caption__term'})]
-    meta_caption_v = [t.text for t in
-                      soup.findAll('dd', {'class':'work__caption__description'})]
-    meta_caption_dct = {k:v for k, v in
-                            zip(meta_caption_k, meta_caption_v)}
+        meta_caption_v = [t.text for t in
+                            soup.findAll('dd',
+                                        {'class':'work__caption__description'})]
+        meta_caption_dct = {k:v for k, v in
+                                zip(meta_caption_k, meta_caption_v)}
 
-    # merge metadata dictionaries
-    meta_dct = {**meta_work_dct, **meta_caption_dct}
+        # merge metadata dictionaries
+        meta_dct = {**meta_work_dct, **meta_caption_dct}
 
-    # clean metadata entries
-    def clean(s:str): return(s.replace('/n', '').strip())
-    meta_dct = {clean(k): clean(v) for k, v in meta_dct.items()}
+        # clean metadata entries
+        def clean(s:str): return(s.replace('/n', '').strip())
+        meta_dct = {clean(k): clean(v) for k, v in meta_dct.items()}
 
-    # include URL in metadata
-    meta_dct['url'] = img_url
+        # include n and URL in metadata
+        meta_dct['id'] = n
+        meta_dct['url'] = img_url
 
-    return meta_dct, img_url
+        return meta_dct, img_url
 
-def write_to_disk(N: int):
+def download_to_disk(n_start: int, n_end: int, dst_fp: str):
     ''' Writes images and metadata for the first N items in the online
         MoMA catalogue to disk. Metadata is written as JSON.
     '''
-    for n in range(1, N+1):
-        img, metadata = retrieve_item(n)
+
+    dst_fp = './data/' + dst_fp
+
+    for n in range(n_start, n_end+1):
+
+        try:
+            meta_dct, img_url = retrieve_item(n)
+            json_str = json.dumps(meta_dct)
+        except TypeError: # fails to unpack objects
+            continue # skip this item
+
+        with open(dst_fp, 'a', encoding='utf8') as file:
+            file.write(json_str + ',')
+
+download_to_disk(1, 5, 'test.json')
